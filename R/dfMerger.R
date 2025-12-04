0f503f938c8bcbc8e0cb6452ef74f3381b62ad26
Emilio Mora <emiliomora92@gmail.com>
2025-12-04 17:14:55 +0100

    in process

---
+++ R/dfMerger.R
@@ -0,0 +1,22 @@
+#' Function to download on the the three files from the WWF Wildfinder in the data repository
+#'
+#' @param csv_name the name of the ".csv" file that we want to download from datarepo/tree/main/data/wildfinder/
+#' @return vector of the files directories in the data/folder
+#' @examples
+#' dl_wildfinder_data()
+
+
+df_small <- read.csv("data/smallData.csv")
+df_world <- read.csv("data/alcohol_per_country.csv")
+
+
+dataFrameMerger <- function(small_data, world_data){
+  dataMerged <- merge(small_data, world_data)
+
+
+
+  data2 = data |>
+    dplyr::group_by(Entity) |>
+    dplyr::summarise(conso = mean(sh_alc_pcap_li))
+  return(data2)
+}
