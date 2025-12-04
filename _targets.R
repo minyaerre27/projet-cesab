059eb3a7c5e1fb2e14b71a70b7399388a143b9b3
Perrine Salvi <salviperrine@gmail.com>
2025-12-04 17:53:31 +0100

    change targets

--- _targets.R
+++ _targets.R
@@ -5,16 +5,21 @@ library(ggplot2)
 targets::tar_source()
 
 list(
+
+  # get path file world data
   tar_target(
     name = data_files,
     command = download_world_data(),
     format = "file"
   ),
   
+  # load file world data
   tar_target(
     name = world_db,
-    command = load_world_data()
+    command = load_data(data_files)
   ),
 
+  # get mean alcohol values per country
   tar_target(mean_data, mean_global(world_db))
+
 )
