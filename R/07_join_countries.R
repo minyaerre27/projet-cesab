5b18347719633f2ca976179c71c5b541616a20cc
Ambre Dechamps <dechampsambre@gmail.com>
2025-12-05 09:35:00 +0100

    update functions name

---
+++ R/07_join_countries.R
@@ -0,0 +1,9 @@
+#' Function to join the countries we found for people with the geographic data of theses countries
+#'
+#' @param spatial_countries the sf object containing the country delimitations and the country codes, computed with the function import_spatial_countries
+#' @param result_countries the table containing the drinking countries of our participants, output of the function ....
+#' @return the sf object containing the country results and their geometry
+#' @examples
+#' join_countries()
+
+join_countries = function(spatial_countries, result_countries) {}
